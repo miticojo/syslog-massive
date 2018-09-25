ff27033cff9779205b78f77db160c21c0748ef6b
Giorgio Crivellari <miticojo@gmail.com>
2018-09-25 08:02:37 +0200

    changed server using goroutine

--- syslog-massive-server.go
+++ syslog-massive-server.go
@@ -1,12 +1,44 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"io"
 	"net"
 	"os"
+	"runtime"
+	"strconv"
 )
+
+func readUDPMsg(connection *net.UDPConn, ch chan string) {
+	buffer := make([]byte, 1024)
+	n, _, err := connection.ReadFromUDP(buffer)
+	if err != nil {
+		panic(err)
+	}
+	ch <- string(buffer[:n])
+
+}
+
+func listenUDP(syslogPort int, syslogProto string) {
+	addr := net.UDPAddr{
+		Port: syslogPort,
+		IP:   net.IP{0, 0, 0, 0},
+	}
+	connection, err := net.ListenUDP("udp", &addr)
+	defer connection.Close()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Listening on %s:%d/%s\n", addr.IP, syslogPort, syslogProto)
+	ch := make(chan string)
+	cnt := 0
+	for i := 0; i < runtime.NumCPU(); i++ {
+		for err == nil {
+			go readUDPMsg(connection, ch)
+			cnt++
+			fmt.Printf("%d - %s", cnt, <-ch)
+		}
+	}
+}
 
 func main() {
 
@@ -17,60 +49,11 @@ func main() {
 		os.Exit(1)
 	}
 
-	syslogPort := args[1]
+	syslogPort, _ := strconv.Atoi(args[1])
 	syslogProto := args[0]
 
-
 	if syslogProto == "udp" {
-		l, err := net.ListenPacket(syslogProto, "0.0.0.0" + ":" + syslogPort)
-
-		if err != nil {
-			fmt.Println("Error listening:", err.Error())
-			os.Exit(1)
-		}
-		// Close the listener when the application closes.
-		defer l.Close()
-		fmt.Printf("Listening for messages on 0.0.0.0 port %s/%s:\n", syslogProto, syslogPort)
-		readUDP(l)
-	} else {
-		l, err := net.Listen("tcp4", "0.0.0.0" + ":" + syslogPort)
-		if err != nil {
-			fmt.Println("Error listening:", err.Error())
-			os.Exit(1)
-		}
-		fmt.Printf("Listening for messages on 0.0.0.0 port %s/%s:\n", syslogProto, syslogPort)
-		conn, _ := l.Accept()
-		defer l.Close()
-		readTCP(conn)
+		listenUDP(syslogPort, syslogProto)
 	}
 }
 
-func readTCP(conn net.Conn){
-	count := 0
-	reader :=  bufio.NewReader(conn)
-	for {
-		message, err := reader.ReadString('\n')
-		//buffer := make([]byte, 2048)
-		//conn.Read(buffer)
-		count++
-		switch err {
-		case nil:
-			break
-		case io.EOF:
-		default:
-			fmt.Println("ERROR", err)
-		}
-		fmt.Printf("%d\t%s", count, message)
-	}
-}
-
-func readUDP(conn net.PacketConn){
-	count := 0
-
-	for {
-		buffer := make([]byte, 2048)
-		conn.ReadFrom(buffer)
-		count++
-		fmt.Printf("%d\t%s", count, string(buffer))
-	}
-}
